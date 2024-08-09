13c025f302437a5b5cbd233dd598379da95555be
Chao <chaz@dtu.dk>
2024-08-09 23:25:05 +0200

    add experiments on vector target function

--- demo.jl
+++ demo.jl
@@ -5,13 +5,14 @@ using Plots
 using NPZ
 using LinearAlgebra
 
+# 1. Parameters
 S = npzread("S.npy")
 M = npzread("M.npy")
 no_of_elements = 128
 ode_reltol = 1e-8
 ode_abstol = 1e-8
 
-###### 2. ODE problem with mass matrix
+# 2. ODE problem with mass matrix
 function dudt!(du, u, p, t)
     du .= S*u
     du[1] = u[1]
@@ -19,12 +20,11 @@ function dudt!(du, u, p, t)
 end
 
 u0 = ones(no_of_elements+1)
-
 f = ODEFunction(dudt!, mass_matrix = M)
 tspan =(0.0, 1.0)
-
-# 1. forward simulation and plot solution
 prob = ODEProblem(f, u0, tspan)
+
+# 3. Forward simulation and plot solution
 sol = solve(prob, FBDF(), reltol = ode_reltol, abstol = ode_abstol, saveat = 0.1) #Rodas5 #FBDF
 
 for i in 1:length(sol.t)
@@ -32,22 +32,47 @@ for i in 1:length(sol.t)
     display(plot(sol.u[i], label="t=$(round(sol.t[i], digits=3))", ylims=(0,1.1), framestyle = :box))
 end
 
-# 2. sensitivity
-prob = ODEProblem(f, u0, tspan)
-function fun_of_interest(u)
+# 4. Sensitivity of scaler function of interest
+function scaler_fun_of_interest(u)
     _prob = remake(prob, u0 = u)
     sol = solve(_prob, FBDF(autodiff=false), reltol = ode_reltol, abstol = ode_abstol) #Rodas5 #FBDF
     return norm(sol.u[end])
 end
 
 # reverse mode
-rs_jac = Zygote.gradient(fun_of_interest, u0)[1]
-println(rs_jac)
+rs_grad = Zygote.gradient(scaler_fun_of_interest, u0)[1]
 
 # forward mode
-fs_jac = ForwardDiff.gradient(fun_of_interest, u0)
-println(fs_jac)
+fs_grad = ForwardDiff.gradient(scaler_fun_of_interest, u0)
 
-# plot sensitivity
-plot(rs_jac, label="reverse", framestyle = :box)
-plot!(fs_jac, label="forward", framestyle = :box)
+# plot gradient
+scalar_plot = plot(rs_grad, label="reverse", framestyle = :box)
+plot!(fs_grad, label="forward", framestyle = :box)
+title!("Gradient of norm of solution")
+display(scalar_plot)
+
+# 5. Sensitivity of vector function of interest
+function vector_fun_of_interest(u)
+    _prob = remake(prob, u0 = u)
+    sol = solve(_prob, FBDF(autodiff=false), reltol = ode_reltol, abstol = ode_abstol) #Rodas5 #FBDF
+    return sol.u[end][1:2:end]
+end
+
+# reverse mode
+rs_jac = Zygote.jacobian(vector_fun_of_interest, u0)[1]
+
+# forward mode
+fs_jac = ForwardDiff.jacobian(vector_fun_of_interest, u0)
+
+# plot jacobian
+plots_layout = @layout [a b; c d]
+p1 = heatmap(rs_jac, label="reverse", framestyle = :box)
+title!("reverse")
+p2 = heatmap(fs_jac, label="forward", framestyle = :box)
+title!("forward")
+p3 = heatmap(rs_jac[:,2:end], label="reverse", framestyle = :box)
+title!("reverse (without 1st col)")
+p4 = heatmap(fs_jac[:,2:end], label="forward", framestyle = :box)
+title!("forward (without 1st col)")
+vector_plot = plot(p1, p2, p3, p4, layout = plots_layout)
+display(vector_plot)
