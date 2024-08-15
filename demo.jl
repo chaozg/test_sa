60146ea261bc67fbc279e6b3c5514f3576409da1
Chao <chaz@dtu.dk>
2024-08-15 23:23:39 +0200

    add maxiters and put back jacobian part

--- demo.jl
+++ demo.jl
@@ -11,6 +11,7 @@ M = npzread("M.npy")
 no_of_elements = 128
 ode_reltol = 1e-12
 ode_abstol = 1e-12
+ode_maxiters = 1e7
 
 # 2. ODE problem with mass matrix
 function dudt!(du, u, p, t)
@@ -36,7 +37,7 @@ end
 # 4. Sensitivity of scaler function of interest
 function scaler_fun_of_interest(u)
     _prob = remake(prob, u0 = u)
-    sol = solve(_prob, Rodas5(autodiff=false), reltol = ode_reltol, abstol = ode_abstol) #Rodas5 #FBDF
+    sol = solve(_prob, Rodas5(autodiff=false), reltol = ode_reltol, abstol = ode_abstol, maxiters=ode_maxiters) #Rodas5 #FBDF
     return norm(sol.u[end])
 end
 
@@ -52,28 +53,28 @@ plot!(fs_grad, label="forward", framestyle = :box)
 title!("Gradient of norm of solution")
 display(scalar_plot)
 
-# # 5. Sensitivity of vector function of interest
-# function vector_fun_of_interest(u)
-#     _prob = remake(prob, u0 = u)
-#     sol = solve(_prob, FBDF(autodiff=false), reltol = ode_reltol, abstol = ode_abstol) #Rodas5 #FBDF
-#     return sol.u[end][1:2:end]
-# end
+# 5. Sensitivity of vector function of interest
+function vector_fun_of_interest(u)
+    _prob = remake(prob, u0 = u)
+    sol = solve(_prob, Rodas5(autodiff=false), reltol = ode_reltol, abstol = ode_abstol, maxiters=ode_maxiters) #Rodas5 #FBDF
+    return sol.u[end][1:2:end]
+end
 
-# # reverse mode
-# rs_jac = Zygote.jacobian(vector_fun_of_interest, u0)[1]
+# reverse mode
+rs_jac = Zygote.jacobian(vector_fun_of_interest, u0)[1]
 
-# # forward mode
-# fs_jac = ForwardDiff.jacobian(vector_fun_of_interest, u0)
+# forward mode
+fs_jac = ForwardDiff.jacobian(vector_fun_of_interest, u0)
 
-# # plot jacobian
-# plots_layout = @layout [a b; c d]
-# p1 = heatmap(rs_jac, label="reverse", framestyle = :box)
-# title!("reverse")
-# p2 = heatmap(fs_jac, label="forward", framestyle = :box)
-# title!("forward")
-# p3 = heatmap(rs_jac[:,2:end], label="reverse", framestyle = :box)
-# title!("reverse (without 1st col)")
-# p4 = heatmap(fs_jac[:,2:end], label="forward", framestyle = :box)
-# title!("forward (without 1st col)")
-# vector_plot = plot(p1, p2, p3, p4, layout = plots_layout)
-# display(vector_plot)
+# plot jacobian
+plots_layout = @layout [a b; c d]
+p1 = heatmap(rs_jac, label="reverse", framestyle = :box)
+title!("reverse")
+p2 = heatmap(fs_jac, label="forward", framestyle = :box)
+title!("forward")
+p3 = heatmap(rs_jac[:,2:end], label="reverse", framestyle = :box)
+title!("reverse (without 1st col)")
+p4 = heatmap(fs_jac[:,2:end], label="forward", framestyle = :box)
+title!("forward (without 1st col)")
+vector_plot = plot(p1, p2, p3, p4, layout = plots_layout)
+display(vector_plot)
